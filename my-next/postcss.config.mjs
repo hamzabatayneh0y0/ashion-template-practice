da05da744e02378254196976a2c2f0afe644aa81
hamzabatayneh0y0 <hamzabatyneh@gmail.com>
2025-06-16 14:29:32 +0300

    sss

--- my-next/postcss.config.mjs
+++ my-next/postcss.config.mjs
@@ -1,12 +1,5 @@
-// const config = {
-//   plugins: ["@tailwindcss/postcss"],
-// };
-
-// export default config;
-// postcss.config.js
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+const config = {
+  plugins: ["@tailwindcss/postcss"],
 };
+
+export default config;

26c4105007f4f0530344aed47551ae80e6f2554c
hamzabatayneh0y0 <hamzabatyneh@gmail.com>
2025-12-20 23:29:36 +0300

    fssd

--- my-next/src/app/CheckOut/page.tsx
+++ my-next/src/app/CheckOut/page.tsx
@@ -17,7 +17,15 @@ interface productType {
 export default async function CheckOut() {
   let products: productType[];
   try {
-    const F = await fetch(`https://fakestoreapi.com/products`, {});
+    const F = await fetch(`https://fakestoreapi.com/products`, {
+      method: "GET",
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+        Accept: "application/json",
+      },
+      next: { revalidate: 3600 },
+    });
     if (!F.ok) {
       throw "fetch error";
     }
